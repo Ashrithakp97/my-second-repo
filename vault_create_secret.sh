e10b52104c6914784226f30e0ea89650fd58a5a8
Ashrithakp97 <ashrithak@suntechnologies.com>
2025-04-29 16:12:28 +0530

    Update vault_create_secret.sh

--- vault_create_secret.sh
+++ vault_create_secret.sh
@@ -2,8 +2,9 @@
 set -e
 
 echo "Logging into Vault using AppRole..."
+# by using role id and secret id vault is logged in
 VAULT_TOKEN=$(vault write -field=token auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID")
-
+# receiving an access pass (the token), which you save in the variable VAULT_TOKEN.
 export VAULT_TOKEN
 
 vault kv put secret/myapp/config username="Ashritha" password="1234"
