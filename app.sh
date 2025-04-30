03efea8c2d929484857eb725469c4f9a52485dab
Ashrithakp97 <ashrithak@suntechnologies.com>
2025-04-30 22:13:09 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -3,9 +3,9 @@ set -e
 set -x
 echo "Using Vault Token: $VAULT_TOKEN"
 vault print token
-#vault kv put secret/app/test username="Ashritha" password="1234"
+vault kv put secret/app/test username="utrfghj" password="1234"
 
-SECRET_OUTPUT=$(vault kv get -field=username secret/app/test)
+#SECRET_OUTPUT=$(vault kv get -field=username secret/app/test)
 
 if [ "$SECRET_OUTPUT" == "Ashritha" ]; then
   echo "Secret created successfully."
