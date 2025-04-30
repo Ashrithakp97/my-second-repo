5fc131a8af149a3232c2c7a976b93519de49a34a
Ashrithakp97 <ashrithak@suntechnologies.com>
2025-04-30 14:11:59 +0530

    Update vault_create_secret.sh

--- vault_create_secret.sh
+++ vault_create_secret.sh
@@ -1,18 +1,9 @@
+#!/bin/bash
 set -e
- 
-echo "Logging into Vault using AppRole..."
-# by using role id and secret id vault is logged in
-VAULT_TOKEN=$(vault write -field=token auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID")
-# receiving an access pass (the token), which you save in the variable VAULT_TOKEN.
-export VAULT_TOKEN
- 
+
+echo "Writing secret to Vault using token from Jenkins..."
+
 vault kv put secret/myapp/config username="Ashritha" password="1234"
- 
-SECRET_OUTPUT=$(vault kv get -field=username secret/myapp/config)
- 
-if [ "$SECRET_OUTPUT" == "Ashritha" ]; then
-  echo "Secret created successfully."
-else
-  echo "Failed to create secret!" >&2  
-  exit 1
-fi
+
+echo "Verifying Vault secret..."
+vault kv get secret/myapp/config
