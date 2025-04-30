78448384f1c54be3c34d0fcbb1f038989bbd61c4
Ashrithakp97 <ashrithak@suntechnologies.com>
2025-04-30 15:26:41 +0530

    Update vault_create_secret.sh

--- vault_create_secret.sh
+++ vault_create_secret.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
-
+VAULT_TOKEN=$(vault write -field=token auth/approle/login role_id="Role ID" secret_id="Secret ID")
+# receiving an access pass (the token), which you save in the variable VAULT_TOKEN.
+export VAULT_TOKEN
 echo "Writing secret to Vault using token from Jenkins..."
 
 vault kv put secret/data/myapp/config username=admin password=123456
