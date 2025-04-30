b24c5223e33efad4bbf2df6f2a12fcdb8a68da9d
Ashrithakp97 <ashrithak@suntechnologies.com>
2025-04-30 16:26:07 +0530

    Create app.sh

---
+++ app.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+ 
+echo "Logging into Vault using AppRole..."
+# by using role id and secret id vault is logged in
+VAULT_TOKEN=$(vault write -field=token auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_ROLE_ID")
+# receiving an access pass (the token), which you save in the variable VAULT_TOKEN.
+export VAULT_TOKEN
+ 
+vault kv put secret/myapp/config username="Ashritha" password="1234"
+ 
+SECRET_OUTPUT=$(vault kv get -field=username secret/myapp/config)
+ 
+if [ "$SECRET_OUTPUT" == "Ashritha" ]; then
+  echo "Secret created successfully."
+else
+  echo "Failed to create secret!" >&2  
+  exit 1
+fi
