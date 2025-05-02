5e5790afa411e63c9f858927d2a1a125f44a14cf
Ashrithakp97 <ashrithak@suntechnologies.com>
2025-05-02 17:03:00 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -5,7 +5,7 @@ set -e
 echo "Vault token is: $VAULT_TOKEN"
 vault login "hvs.CAESIFA9diGjjWdiRoIHU-t-9A6Gv1FvBPQ16WNA0h8A4VDUGh4KHGh2cy5tbm5SNnlUWWM2aENLQTdOQTE2ZFkwVjg"
 echo "Creating a test secret..."
-vault secrets enable -path=ashi -version=2 kv
+vault secrets enable -path=tech -version=2 kv
 
 vault kv put ashi/techapp username="admin" password="123"
 
