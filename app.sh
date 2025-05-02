110ad3d54764a7d1b95802dee66e70daea51edac
Ashrithakp97 <ashrithak@suntechnologies.com>
2025-05-02 16:23:46 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -4,7 +4,8 @@
 set -e
 
 echo "Creating a test secret..."
+vault secrets enable -path=kv-v2
 
-vault kv put secret/app/test username="admin" password="password123"
+vault kv put kv-v2/app/test username="admin" password="password123"
 
 echo "KV secrets engine setup complete."
