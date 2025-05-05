3c32ec0ff7a0436360e0e550fc66c498d75f7dba
Ashrithakp97 <ashrithak@suntechnologies.com>
2025-05-05 16:32:10 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -2,11 +2,13 @@
 
 # Fail on any error
 set -e
-echo "Vault token is: $VAULT_TOKEN"
-vault login "hvs.CAESIFA9diGjjWdiRoIHU-t-9A6Gv1FvBPQ16WNA0h8A4VDUGh4KHGh2cy5tbm5SNnlUWWM2aENLQTdOQTE2ZFkwVjg"
+export VAULT_ADDR="http://65.2.69.108:8200"
+
+export VAULT_TOKEN=$(cat ./vault_token.txt)
+#vault login "hvs.CAESIFA9diGjjWdiRoIHU-t-9A6Gv1FvBPQ16WNA0h8A4VDUGh4KHGh2cy5tbm5SNnlUWWM2aENLQTdOQTE2ZFkwVjg"
 echo "Creating a test secret..."
 vault secrets enable -path=tech -version=2 kv
 
-vault kv put ashi/techapp username="admin" password="123"
+vault kv put sun/testing username="admin" password="123"
 
 echo "KV secrets engine setup complete."
