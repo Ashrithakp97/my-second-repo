b4fff6d913f0c9846e89a62872ada63e01c5bd32
Ashrithakp97 <ashrithak@suntechnologies.com>
2025-05-06 15:47:27 +0530

    Update vault-read.sh

--- vault-read.sh
+++ vault-read.sh
@@ -3,6 +3,10 @@
 set -e
 
 echo "Using VAULT_ADDR: $VAULT_ADDR"
+ROLE_ID="$ROLE_ID"
+SECRET_ID="$SECRET_ID"
+LOGIN_RESPONSE=$(vault write -format=json auth/approle/login role_id="$ROLE_ID" secret_id="$SECRET_ID")
+VAULT_TOKEN=$(echo $LOGIN_RESPONSE)
 echo "Reading Vault secret at path: secret/myapp/config"
 
 # Uses VAULT_TOKEN automatically injected by Jenkins Vault plugin
