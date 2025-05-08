0916f0d932d01e989e4371402933e02c3a670760
Ashrithakp97 <ashrithak@suntechnologies.com>
2025-05-08 18:40:00 +0530

    Update createsec.sh

--- createsec.sh
+++ createsec.sh
@@ -4,7 +4,7 @@ echo "Logging into Vault via AppRole..."
 
 VAULT_TOKEN=$(vault write -field=token auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID")
 export VAULT_TOKEN
-wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${VAULT_TOKEN}", var: 'C_VAULT_TOKEN']]])
+
 
 echo "Creating secret at secret/myapp/config..."
 vault kv put secret/myapp/config username=ashri password=123
