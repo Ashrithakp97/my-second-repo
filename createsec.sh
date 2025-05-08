d44c24256706a64dd2f1a74709ad82b09bda758f
Ashrithakp97 <ashrithak@suntechnologies.com>
2025-05-08 19:31:48 +0530

    Update createsec.sh

--- createsec.sh
+++ createsec.sh
@@ -4,7 +4,6 @@ echo "Logging into Vault via AppRole..."
 
 VAULT_TOKEN=$(vault write -field=token auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID")
 export VAULT_TOKEN
-echo VAULT_TOKEN
 
 echo "Creating secret at secret/myapp/config..."
 vault kv put secret/myapp/config username=ashri password=123
